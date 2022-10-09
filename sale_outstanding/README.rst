867e3af50a24d4d0bda9053e46505c3e987bff3f
Stéphan Sainléger <contact@stephansainleger.fr>
2022-10-09 11:27:29 +0200

    [ADD] create `sale_outstanding` add-on

---
+++ sale_outstanding/README.rst
@@ -0,0 +1,44 @@
+================
+sale_outstanding
+================
+
+Calculates and displays the sale order outstanding and pending work.
+
+Installation
+============
+
+Use Odoo normal module installation procedure to install
+``sale_outstanding``.
+
+Known issues / Roadmap
+======================
+
+None yet.
+Bug Tracker
+===========
+
+Bugs are tracked on `our issues website <https://github.com/elabore-coop/sale-tools/issues>`_. In case of
+trouble, please check there if your issue has already been
+reported. If you spotted it first, help us smashing it by providing a
+detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Stéphan Sainléger
+
+Funders
+-------
+
+The development of this module has been financially supported by:
+* Elabore (https://elabore.coop)
+* Datactivist (https://datactivist.coop)
+
+
+Maintainer
+----------
+
+This module is maintained by Elabore.
---
+++ sale_outstanding/models/sale.py
@@ -0,0 +1,21 @@
+from odoo import _, api, fields, models
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.depends("order_line.qty_delivered", "order_line.qty_invoiced", "order_line.qty_to_invoice", "order_line.price_unit")
+    def _compute_sum_outstanding(self):
+        for order in self:
+            lines_outstanding = order.order_line.mapped(lambda r:(r.qty_to_invoice * r.price_unit))
+            order["sum_outstanding"] = sum(lines_outstanding)
+
+    @api.depends("order_line.qty_delivered", "order_line.product_uom_qty", "order_line.price_unit")
+    def _compute_sum_pending_work(self):
+        for order in self:
+            lines_pending_work = order.order_line.mapped(lambda r:((r.product_uom_qty - r.qty_delivered) * r.price_unit))
+            order["sum_pending_work"] = sum(lines_pending_work)
+
+
+    sum_outstanding = fields.Monetary(_("Outstanding Untaxed"), readonly=True, store=True, compute="_compute_sum_outstanding")
+    sum_pending_work = fields.Monetary(_("To Do Untaxed"), readonly=True, store=True, compute="_compute_sum_pending_work")
+
